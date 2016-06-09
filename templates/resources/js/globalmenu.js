098481936df7c42ee438bc68501e6c60e44e355f
Carsten Thiel <thiel@sub.uni-goettingen.de>
2016-06-09 11:26:31 +0200

    don't allow links on submenu headers

--- templates/resources/js/globalmenu.js
+++ templates/resources/js/globalmenu.js
@@ -7,44 +7,44 @@ $.getJSON( 'https://res.de.dariah.eu/globalmenu/menu.json', function( data ) {
   // replace the existing menu with an empty one
   $( 'ul#home_dropdown_menu' ).replaceWith( '<ul id="home_dropdown_menu" class="dropdown-menu"></ul>' );
 
-  var lifromarrayitem = function (arrayitem) {
+  var liFromArrayItem = function (arrayItem) {
     // create an empty li
-    var myobject = document.createElement('li');
-    var linktarget, tabindexstring, classString;
-    if ('divider' in arrayitem) {
+    var myObject = document.createElement('li');
+    var linkTarget, tabIndexString, classString;
+    if ('divider' in arrayItem) {
       // make it a divider
-      $(myobject).addClass('divider');
+      $(myObject).addClass('divider');
     } else {
-      // check whether there is a link
-      if ('link' in arrayitem) {
-        linktarget = arrayitem['link'];
-        tabindexstring = '';
+      // check whether there is a link and not a submenu
+      if ('link' in arrayItem && !('submenu' in arrayItem)) {
+        linkTarget = arrayItem['link'];
+        tabIndexString = '';
         classString = 'class="navtarget"';
       } else {
-        linktarget = '#';
-        tabindexstring = 'tabindex="-1"';
+        linkTarget = '#';
+        tabIndexString = 'tabindex="-1"';
         classString = '';
       }
       // add the link inside
-      $(myobject).html('<a '+ tabindexstring +' href="' + linktarget + '" '+classString+'>' + arrayitem['title'] + '</a>');
+      $(myObject).html('<a '+ tabIndexString +' href="' + linkTarget + '" '+classString+'>' + arrayItem['title'] + '</a>');
       // if the li has a submenu, add another ul
-      if ('submenu' in arrayitem) {
-        $(myobject).addClass('dropdown-submenu');
+      if ('submenu' in arrayItem) {
+        $(myObject).addClass('dropdown-submenu');
         var newsubmenu = document.createElement('ul');
         $(newsubmenu).addClass('dropdown-menu');
         // for each cild, recurse!
-        $.each(arrayitem['submenu'], function (arrayid, arrayitem) {
-          $(newsubmenu).append(lifromarrayitem(arrayitem));
+        $.each(arrayItem['submenu'], function (arrayId, arrayItem) {
+          $(newsubmenu).append(liFromArrayItem(arrayItem));
         });
-        $(myobject).append(newsubmenu);
+        $(myObject).append(newsubmenu);
       }
     }
-    return myobject;
+    return myObject;
   };
 
   // loop through the outer array and append childs to the new ul from above
-  $.each( data['menu'], function( arrayid, arrayitem ) {
-    $( 'ul#home_dropdown_menu' ).append( lifromarrayitem(arrayitem) );
+  $.each( data['menu'], function( arrayId, arrayItem ) {
+    $( 'ul#home_dropdown_menu' ).append( liFromArrayItem(arrayItem) );
   });
 
 }).fail(function() {
@@ -52,7 +52,7 @@ $.getJSON( 'https://res.de.dariah.eu/globalmenu/menu.json', function( data ) {
 });
 
 
-// submenu workaround for mobiles. 
+// submenu workaround for mobiles.
 $(function() {
   $('.navbar-dariah').on('touchstart click tap', 'li.dropdown-submenu > a', function(e) {
     e.preventDefault(); // do not aktivate links
